849ddf2df9b5e3447868fe9b88d66f2f2b944d3d
airyou-code <holod7272@outlook.com>
2022-09-25 16:40:06 +0200

    change admin setings

--- blog/films/admin.py
+++ blog/films/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Film, Year, Age, Country, Ganer, Type, Persons, Group, Comment
 
-admin.site.register(Film)
+
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ['id','id_film', 'name' ]
+    search_fields = ['id_film', 'name']
+    list_filter = ['tg_genre']
+
+admin.site.register(Film, FilmAdmin)
 admin.site.register(Persons)
 admin.site.register(Comment)
 # admin.site.register(Year)
