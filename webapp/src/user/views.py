4b0b8a5136e43c7a49fea20fb5681a6508afbbc9
airyou-code <holod7272@outlook.com>
2022-10-25 22:22:32 +0200

    html

--- webapp/src/user/views.py
+++ webapp/src/user/views.py
@@ -7,19 +7,18 @@ from .forms import LoginForm, UserRegistrationForm
 
 def user_login(request):
     if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(username=cd['username'], password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponseRedirect(f"/")
-                    return HttpResponse('Authenticated successfully')
-                else:
-                    return HttpResponse('Disabled account')
+        name = request.POST.get('name')
+        pasword = request.POST.get('pasword')
+        user = authenticate(username=name, password=pasword)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(f"/")
+                return HttpResponse('Authenticated successfully')
             else:
-                return HttpResponse('Invalid login')
+                return HttpResponse('Disabled account')
+        else:
+            return HttpResponse('Invalid login')
     else:
         form = LoginForm()
     return render(request, 'user/login.html', {'form': form})
